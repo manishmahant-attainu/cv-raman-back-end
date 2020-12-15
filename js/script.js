497e5f4d19589c0922e0d89a58b764293270fe0c
Manish Mahant <manishmahant>
2020-12-15 11:09:25 +0530

    js intro and data types

---
+++ js/script.js
@@ -0,0 +1,44 @@
+'use strict';
+//declaring
+var firstName;
+console.log(typeof firstName);
+console.log(firstName);
+
+//initialising
+firstName = "Manish";
+console.log(typeof firstName);
+console.log(firstName);
+
+var lastName = "Mahant";
+console.log(typeof lastName);
+console.log(lastName);
+
+var year = 2020;
+console.log(typeof year);
+console.log(year);
+
+var temperature = 25.67;
+console.log(typeof temperature);
+console.log(temperature);
+
+var above18 = true;
+console.log(typeof above18);
+console.log(above18);
+
+var personalInfo = {
+    firstName: "Manish",
+    lastName: "Manish"
+};
+console.log(typeof personalInfo);
+console.log(personalInfo);
+
+var tasks = [
+    121,
+    "Go for a movie"
+];
+console.log(typeof tasks);
+console.log(tasks);
+
+var myAge = null;
+console.log(typeof myAge);
+console.log(myAge);
