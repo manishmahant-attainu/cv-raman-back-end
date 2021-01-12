dc4d0e2b61825aaeeec83fa4d075d8a8d7986f87
Manish Mahant <manishmahant>
2021-01-12 15:32:38 +0530

    node core modules

--- src/file.js
+++ src/file.js
@@ -1,21 +1,27 @@
 const fs = require('fs');
 
 const filePath = '/Users/manishmahant/Sites/AttainU/cv-raman-back-end/src/hello.text';
+
+//Synchronous way of reading the file
 try {
     const fileData = fs.readFileSync(filePath,'utf-8');
-    console.log(fileData);
+    // console.log(fileData);
 } catch (error) {
     console.log(error);
 }
 
+//Async way of reading the file
 fs.readFile(filePath,'utf-8',function(error, data){
     if(error) {
         console.log(error)
     } else {
-        console.log(data)
+        // console.log(data)
     }
 });
 
+// fs.writeFile(filePath,'Writing with nodejs FS module',function(err){
+//     console.log(err);
+// })
 
 //Synchronous
 console.log(1)
@@ -40,3 +46,66 @@ setTimeout(()=>{
 setTimeout(()=>{
     console.log(1000)
 },1000);
+
+
+//RAM
+const b = Buffer.alloc(10);
+
+b.write('This is cv raman batch and we are learning NodeJs');
+console.log(b);
+
+const c = Buffer.from('This is cv raman batch and we are learning NodeJs');
+console.log(c);
+
+var fileData = '';
+//create read stream
+const rs = fs.createReadStream(filePath);
+rs.setEncoding('utf-8');
+
+
+//1 when we receive the data
+//2 when the data is completely recevied, end 
+//3 when there is an error
+//4 when the data transfer finishes, finish
+
+rs.on('data', function(chunk){
+    // console.log(chunk);
+    console.log('======================================');
+    fileData += chunk;
+})
+
+rs.on('error', function(error){
+    console.log(error);
+})
+
+rs.on('end', function(){
+    // console.log(fileData);
+})
+
+rs.on('finish', function(){
+    console.log('finished');
+});
+
+let interval = ''; 
+
+// setTimeout
+var timeout = setTimeout(function(){
+    console.log('Clearing Interval')
+    clearInterval(interval);
+},10000);
+
+
+// setInterval
+interval = setInterval(function(){
+    console.log(Date())
+},1000);
+console.log('interval',interval);
+
+// setInterval(function(){
+//     console.log('ssaas',Date())
+// },1000);
+
+var aa = [1,2,3,4,5];
+
+// clearTimeout(timeout);
+
--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
 import constants, { PORT } from './constants';
-const server = require('express');
-const app = server();
+// const server = require('express');
+// const app = server();
 // console.log(app);
 
 //createServer function requires a callback(function which can accept params)
+const http = require('http');
+const server = http.createServer((req,resp)=>{
+    // console.log(req.url);
+    // if(req.url === '/phones') {
+    //     resp.setHeader('Content-Type','application/json');
+    //     resp.end(JSON.stringify(constants));
+    // }
+    resp.end('<h1>Some text content</h1>');
+});
 
-// const server = http.createServer((req,resp)=>{
-//     console.log(req.url);
-//     if(req.url === '/phones') {
-//         resp.setHeader('Content-Type','application/json');
-//         resp.end(JSON.stringify(constants));
-//     }
-//     resp.end('<h1>Some text content</h1>');
-// });
-
-// server.listen(PORT, ()=>{
-//     console.log(`Listening to: http://localhost:${PORT}`)
-// });
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/about-me', (req, res) => {
-    console.log(req.url);
-    res.json(constants)
-})
-
-app.get('/projects', (req, res) => {
-    res.json(constants)
-})
-
-app.get('/contact-me', (req, res) => {
-    res.json(constants)
-})
-
-app.get('*', (req, res) => {
-    res.send('Page Not Found')
-})
-
-app.listen(PORT, 'localhost', ()=>{
+server.listen(PORT, ()=>{
     console.log(`Listening to: http://localhost:${PORT}`)
 });
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.get('/about-me', (req, res) => {
+//     console.log(req.url);
+//     res.json(constants)
+// })
+
+// app.get('/projects', (req, res) => {
+//     res.json(constants)
+// })
+
+// app.get('/contact-me', (req, res) => {
+//     res.json(constants)
+// })
+
+// app.get('*', (req, res) => {
+//     res.send('Page Not Found')
+// })
+
+// app.listen(PORT, 'localhost', ()=>{
+//     console.log(`Listening to: http://localhost:${PORT}`)
+// });
