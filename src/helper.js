a96538a92c433e4183ed5f62347374bdc49f29b7
Manish Mahant <manishmahant>
2021-01-18 10:04:10 +0530

    Promises and Callback

--- src/helper.js
+++
@@ -1,7 +0,0 @@
-const sum = (a,b) => a+b;
-const sub = (a,b) => a-b;
-
-// export default {
-//     sum,
-//     SUB:sub,
-// }
--- src/index.js
+++ src/index.js
@@ -1,44 +1,80 @@
-import constants, { PORT } from './constants';
-// const server = require('express');
-// const app = server();
-// console.log(app);
+import 'isomorphic-fetch';
+// What is a callback? Its a function passed as an argument, which may or may not be called.
+// Why callback is used? For achievieing async behaviour.
+// Callback hell? callbacks inside callbacks
+function c3() {
 
-//createServer function requires a callback(function which can accept params)
-const http = require('http');
-const server = http.createServer((req,resp)=>{
-    // console.log(req.url);
-    // if(req.url === '/phones') {
-    //     resp.setHeader('Content-Type','application/json');
-    //     resp.end(JSON.stringify(constants));
-    // }
-    resp.end('<h1>Some text content</h1>');
+}
+
+function c2(c) {
+    c()
+}
+
+function c1(c) {
+    c(c3);
+}
+
+function m1(c) {
+    c(c2);
+}
+
+m1(c1);
+
+// Promises (ES6)
+// 3 states
+//  1. Pending
+//  2. resolved
+//  3. rejected
+
+const p = new Promise(function(resolve,reject){
+    // console.log(typeof resolve)
+    setTimeout(() => {
+        reject('manish');    
+    }, 6000);
+    setTimeout(() => {
+        resolve(2);    
+    }, 5000);
+    
+});
+//Success Handling
+p.then(r=>{
+    console.log(r)
+    return r*2;
+}).then(r=>{
+    console.log(r)
+    return r*4;
+})
+
+//Error handling
+p.catch(e=>{
+    console.log(e);
 });
 
-server.listen(PORT, ()=>{
-    console.log(`Listening to: http://localhost:${PORT}`)
-});
+console.log(p);
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+function info(j) {
+    console.log('info');
+    console.log(j);
+}
 
-// app.get('/about-me', (req, res) => {
-//     console.log(req.url);
-//     res.json(constants)
-// })
+function mainFunc(i) {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => i(json))
+        .catch(e => i(e))
+}
 
-// app.get('/projects', (req, res) => {
-//     res.json(constants)
-// })
+mainFunc(info);
+console.log('anything')
 
-// app.get('/contact-me', (req, res) => {
-//     res.json(constants)
-// })
-
-// app.get('*', (req, res) => {
-//     res.send('Page Not Found')
-// })
-
-// app.listen(PORT, 'localhost', ()=>{
-//     console.log(`Listening to: http://localhost:${PORT}`)
-// });
+function pr(url){
+    return new Promise(function(resolve,reject){
+        setTimeout(() => {
+            resolve(url);
+        }, 2000);
+    })
+}
+pr('https://jsonplaceholder.typicode.com/todos/1')
+ .then(function(response){
+    console.log(response);
+})
