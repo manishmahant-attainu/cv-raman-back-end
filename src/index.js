e9a6284dee4c6255879be4fce964888a220bd77d
Manish Mahant <manishmahant>
2021-01-22 07:55:44 +0530

    middlewares

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 import { users } from './constants';
 const app = express();
 const PORT = 5002;
+const signedUsers = [];
+const openUrlCount = {};
+// custom middleware || URL Logger
+// it must fullfil few things that it must accept 3 params
+//  1st param is your request
+//  2nd param is your response
+//  3rd param is next function
+
+// Middlewares are registered in the sequential order
+const urlLogger = (request,response,next) => {
+    openUrlCount[request.url] = openUrlCount[request.url]
+        ? openUrlCount[request.url]+1
+        : 1
+    if(request.body.name === 'Manish Mahant') {
+        throw new Error('Manish is not allowed');
+        // response.send({
+        //     message: 'Manish is not allowed'
+        // })
+    } else {
+        request.body.openUrlCount= openUrlCount;
+        next();
+    }
+}
+
+// A callback is actually a function which can be passed as a parameter inside other function
+// this callback function may or may not be execited form the function where it has been passed
+// this function can pass some parameters to the callback
 
 //Defining static assets
 app.use(express.static(path.join(__dirname,'assets')));
+
+
+// n umber of middlewares in express 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(urlLogger);
 
 // same path + same method
 //Route 1
@@ -15,11 +49,21 @@ app.get('/', (req, res) => {
     })
 });
 
+app.get('/getUrlOpen', (req, res) => {
+    res.json(openUrlCount)
+});
+
+
 //Route 2
-app.post('/', (req, res) => {
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    signedUsers.push(req.body)
     res.json({
         message: 'posted successfully'
-    })
+    });
+});
+app.get('/susers', (req, res) => {
+    res.json(signedUsers);
 });
 
 //Route 3
@@ -63,6 +107,13 @@ app.get('/home', (req,res) => {
     const homeFilePath = path.join(__dirname,'home.html');
     res.sendFile(homeFilePath)
 });
+
+
+//In express parameters count plays a very important role
+app.use((error, request, response, next)=>{
+    console.log('Error: handler',error)
+    response.status(400).send(error)
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on: http://localhost:${PORT}`);
