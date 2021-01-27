fa27759aced71ff20a6f4b733c669641c6d5a59b
Manish Mahant <manishmahant>
2021-01-27 11:47:15 +0530

    handlebars templating

--- src/index.js
+++ src/index.js
@@ -1,122 +1,29 @@
-import express from 'express';
 import path from 'path';
+import express from 'express';
 import bodyParser from 'body-parser';
-import { users } from './constants';
+
 const app = express();
 const PORT = 5002;
-const signedUsers = [];
-const openUrlCount = {};
-// custom middleware || URL Logger
-// it must fullfil few things that it must accept 3 params
-//  1st param is your request
-//  2nd param is your response
-//  3rd param is next function
+const users = [];
 
-// Middlewares are registered in the sequential order
-const urlLogger = (request,response,next) => {
-    openUrlCount[request.url] = openUrlCount[request.url]
-        ? openUrlCount[request.url]+1
-        : 1
-    if(request.body.name === 'Manish Mahant') {
-        throw new Error('Manish is not allowed');
-        // response.send({
-        //     message: 'Manish is not allowed'
-        // })
-    } else {
-        request.body.openUrlCount= openUrlCount;
-        next();
-    }
-}
-
-// A callback is actually a function which can be passed as a parameter inside other function
-// this callback function may or may not be execited form the function where it has been passed
-// this function can pass some parameters to the callback
-
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views',path.join(__dirname,'views'));
 app.set("view engine", "hbs");
+
+const layout = path.join('layouts','index');
 
 //Defining static assets
 app.use(express.static(path.join(__dirname,'assets')));
 
 
-// n umber of middlewares in express 
+// number of middlewares in express 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(urlLogger);
 
-// same path + same method
-//Route 1
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome to your main api endpoint'
-    })
+app.get('/',(req,res)=>{
+    res.end('welcome to express');
 });
 
-app.get("/players", function(request, response) {
-    response.render("players.hbs");
-});
-
-app.get('/getUrlOpen', (req, res) => {
-    res.json(openUrlCount)
-});
-
-
-//Route 2
-app.post('/signup', (req, res) => {
-    console.log(req.body);
-    signedUsers.push(req.body)
-    res.json({
-        message: 'posted successfully'
-    });
-});
-app.get('/susers', (req, res) => {
-    res.json(signedUsers);
-});
-
-//Route 3
-app.get('/', (req, res) => {
-    res.send('hello world')
-});
-
-//Route 4
-app.get('/profile', (req,res) => {
-    res.json({
-        firstName: "Manish",
-        lastName: "Mahant",
-        gender: "Male",
-    })
-});
-
-app.get('/users',  (req,res) => {
-    res.json(users);
-});
-
-app.get('/users/:id/abc/:type',  (req,res) => {
-    console.log('query',req.query);
-    console.log('params',req.params);
-    const user = users.filter(
-        // function(i) {
-        //     return i.id == req.params.id
-        // }
-        i => i.id == req.params.id && i.type == req.params.type
-    );
-    if(user.length) {
-        res.json(user[0].name);
-    } else {
-        res.json({
-            message: 'User not found'
-        });
-    }
-    
-});
-
-app.get('/home', (req,res) => {
-    const homeFilePath = path.join(__dirname,'home.html');
-    res.sendFile(homeFilePath)
-});
-
-
-app.get('/hbshome',(req,res)=>{
+app.get('/intro',(req,res)=>{
     const homeFilePath = path.join(__dirname,'home.hbs');
     const data = {
         isLoggedIn:true,
@@ -129,14 +36,42 @@ app.get('/hbshome',(req,res)=>{
         ]
     }
     res.render(homeFilePath,data);
-})
+});
 
+app.get('/signup',(req,res)=> {
+    const data = {
+        layout:layout,
+        users,
+        title: 'Sign Up',
+        name: '',
+        email: '',
+        address: '',
+    };
+    res.render('signup.hbs',data);
+});
 
-//In express parameters count plays a very important role
-app.use((error, request, response, next)=>{
-    console.log('Error: handler',error)
-    response.status(400).send(error)
-})
+app.post('/signup',(req,res)=> {
+    const error = {};
+    const data = {
+        layout,
+        title: 'Sign Up',
+        ...req.body
+    }
+    if(!req.body.name) {
+        error.name = 'Please enter the name';
+        res.render('signup',{...data,error});
+        return;
+    }
+    users.push(req.body);
+    res.redirect('/signup');
+});
+
+app.get('/users',(req,res)=>{
+    res.render('users.hbs',{
+        title:'User Listing',
+        users
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on: http://localhost:${PORT}`);
